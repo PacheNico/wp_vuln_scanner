7986ef562956a6d05180cf6420396c9602dbc881
NicoPache <mnpache@gmail.com>
2020-04-22 22:59:19 -0400

    API working.
    To-do: itterate json and download zip files to a folder

--- WP_API/plugin_api.py
+++ WP_API/plugin_api.py
@@ -2,19 +2,33 @@ import requests
 import json
 
 
-API_URL = "http://api.wordpress.org/plugins/info/1.0/?action=query_plugins";
+API_URL = "http://api.wordpress.org/plugins/info/1.1/?action=query_plugins";
+
 
 fields = {
-    "description": False, 
-    "sections": False, 
-    "tested": False,
-    "requires": False,
-    "rating": False,
-    "downloaded": True,
-    "downloadlink": True,
-    "last_updated": False,
-    "homepage": False,
-    "tags": False
+        "name" : 0,
+        "author" : 0,
+        "slug" : 0,
+        "downloadlink" : 0,
+        "rating" : 0,
+        "ratings" : 0,
+        "downloaded" : 0,
+        "description" : 0,
+        "active_installs" : 0,
+        "short_description" : 0,
+        "donate_link" : 0,
+        "tags" : 0,
+        "sections" : 0,
+        "homepage" : 0,
+        "added" : 0,
+        "last_updated" : 0,
+        "compatibility" : 0,
+        "tested" : 0,
+        "requires" : 0,
+        "versions" : 0,
+        "support_threads" : 0,
+        "support_threads_resolved" : 0
+
 }
 query_plugins = {
     "browse":"popular",
@@ -22,22 +36,33 @@ query_plugins = {
     "tag":"",
     "author":"",
     "page":1,
-    "per_page":100,
-    "fields":fields
-    }
-
-
-
-plugin_information = {
-    "slug":"",
+    "per_page":1,
     "fields":fields
 }
+
+def url_helper(parentlist,dictionary):
+    param = ""
+    for key, value in dictionary.items():
+        if type(value) is dict:
+            param= param+ url_helper(parentlist+"["+key+"]",value)
+        else:
+            param= param+ parentlist+"["+str(key)+"]="+str(value)+"&"
+
+    return param
+
+def buildUrl():
+    url = API_URL +"&"+ url_helper("request",query_plugins)[:-1]
+    print("URL:"+ url +"\n\n\n---------------------------------------------------")
+    return url
 
 def getPluginInfo(plugin):
     print(plugin)
 
 def getPlugins():
-    r = requests.post(url = API_URL, data = json.dumps(query_plugins)) 
-    print("request response:%s"%r.text) 
+    url = buildUrl();
+    r = requests.post(url = url)
+    response_json = json.loads(r.text)
+    json_formatted_str = json.dumps(response_json, indent=2)
+    print(json_formatted_str)
 
 getPlugins()
