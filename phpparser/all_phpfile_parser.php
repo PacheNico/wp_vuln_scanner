f61348e926975e28a15d6760fdbecfb2425ae1a5
Sharith Godamanna <sharithg@bu.edu>
2020-04-25 20:51:45 -0400

    parses all files, and returs variable that contains a sql statement

--- phpparser/all_phpfile_parser.php
+++ phpparser/all_phpfile_parser.php
@@ -4,6 +4,11 @@ require 'vendor/autoload.php';
 use PhpParser\Error;
 use PhpParser\NodeDumper;
 use PhpParser\ParserFactory;
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Function_;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitorAbstract;
+use PhpParser\NodeFinder;
 
 function getDirContents($dir, &$results = array()) {
     $files = scandir($dir);
@@ -26,15 +31,32 @@ function getDirContents($dir, &$results = array()) {
     return $results;
 }
 
-//var_dump(getDirContents('testfolder/'));
+class ParentConnector extends NodeVisitorAbstract {
+    private $stack;
+    public function beforeTraverse(array $nodes) {
+        $this->stack = [];
+    }
+    public function enterNode(Node $node) {
+        if (!empty($this->stack)) {
+            echo "test";
+            $node->setAttribute('parent', $this->stack[count($this->stack)-1]);
+        }
+        $this->stack[] = $node;
+    }   
+    public function leaveNode(Node $node) {
+        array_pop($this->stack);
+    }
+}
 
 function parser($directory){
-
     $all_php_paths = getDirContents($directory);
 
     foreach ($all_php_paths as $key => $value) {
+        // extending NodeVisitorAbstract to store the parent class
         $links_contents = file_get_contents($value);
         $code = $links_contents;
+
+        // obtain AST
         $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
         try {
             $ast = $parser->parse($code);
@@ -43,9 +65,44 @@ function parser($directory){
             return;
         }
 
+    // begin traverser
+        $traverser = new NodeTraverser();
+        $traverser->addVisitor(new class extends NodeVisitorAbstract {
+
+            private $stack;
+            public $varName;
+            public function beforeTraverse(array $nodes) {
+                $this->stack = [];
+            }
+
+            public function enterNode(Node $node) {
+                // $dumper = new NodeDumper;
+                // echo $dumper->dump($node) . "\n\n\n";
+
+                if (!empty($this->stack)) {
+                    $node->setAttribute('parent', $this->stack[count($this->stack)-1]);
+                }
+                $this->stack[] = $node;
+
+                if ($node instanceof Node\Identifier && $node->name == "query") {
+                    $queryName = $node->name;
+                    $queryArgs = $node->getAttribute('parent')->args;
+                    $varName = array_values($queryArgs)[0]->value->name;
+                    echo $varName . "\n";
+                }
+            }
+
+            public function leaveNode(Node $node) {
+                array_pop($this->stack);
+            }
+
+        });
+
+        // traverse AST
         $dumper = new NodeDumper;
-        echo $dumper->dump($ast) . "\n";
+        $ast = $traverser->traverse($ast);
     }
+
 }
 
 parser($argv[1])
