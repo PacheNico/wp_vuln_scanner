9f13bf60ec3e2707ce774dcbb96a7575f59ecdd4
NicoPache <mnpache@gmail.com>
2020-04-26 02:09:57 -0400

    cleanup and testing

--- WP_API/plugin_api.py
+++ WP_API/plugin_api.py
@@ -1,15 +1,14 @@
 import requests
 import json
 import urllib.request
-import threading
-import os
-import subprocess
 import concurrent.futures
+import os
 
-ALL_VERSION = False #download all version of code available
-DEFAULT_LOCATION = "/tmp/testfolder" #default download folder
-THREAD_COUNT=10
-
+# configurable variables
+ALL_VERSION = False         #download all version of code available
+DEFAULT_LOCATION = "/tmp/testfolder"  #default download folder
+THREAD_COUNT=7 #threads in threadpool
+PLUGINS_PER_BATCH = 75 #number of plugins per page 
 API_URL = "http://api.wordpress.org/plugins/info/1.1/?action=query_plugins";
 
 #fields returned by API (SOME ALWAYS ON)
@@ -39,16 +38,17 @@ fields = {
 
 }
 
-#params structure
+#REQUEST structure
 query_plugins = {
     "browse":"popular",
     "search":"",
     "tag":"",
     "author":"",
-    "per_page":100, #250 MAX
+    "per_page":PLUGINS_PER_BATCH, #250 MAX
     "fields":fields
 }
 
+#helps builds the url based on the query plugin and fields variable 
 def url_helper(parentlist,dictionary):
     param = ""
     for key, value in dictionary.items():
@@ -59,26 +59,26 @@ def url_helper(parentlist,dictionary):
 
     return param
 
+# returns the url need to get a page of plugins
 def buildUrl(page_num=1):
     url = API_URL +"&"+ url_helper("request",query_plugins)+"request[page]="+str(page_num)
-    #print("URL:"+ url +"\n\n\n---------------------------------------------------")
     return url
 
-
+#makes a post request to the API and returns the list of plugins 
 def getPlugins(page_num=1):
     url = buildUrl(page_num);
     r = requests.post(url = url)
     response_json = json.loads(r.text)
-    json_formatted_str = json.dumps(response_json, indent=2)
-    #print(json_formatted_str)
     return response_json
 
+#downloads from a url to the loc folder with the given name
 def download_from_link(url,name,loc):
     try:
         urllib.request.urlretrieve(url,loc+name)
     except:
-        print("download failed: "+name)
+        print("download failed: "+name) #need to fix issue of wierd characters in some plugin names 
 
+#downloads all the plugins in the returned list of plugins 
 def downloadPlugins(plugins,download_loc):
     print('Beginning plugins download')
     for x in plugins['plugins']:
@@ -87,11 +87,11 @@ def downloadPlugins(plugins,download_loc):
             for y in x['versions']:
                 download_from_link(x['versions'][y],x['name']+y+".zip",download_loc)
 
-    print('plugin download complete')
-
+#gets the number of pages in the WP API
 def getPageCount():
    return getPlugins()["info"]["pages"]
 
+#defines the process needed to analyze a page of the plugins
 def threading_process(page_num):
     plugins= getPlugins(page_num)
     download_loc = DEFAULT_LOCATION +"/"+str(page_num)+"/"
@@ -103,12 +103,13 @@ def threading_process(page_num):
     unpack = subprocess.check_call("./unpack '%s'" % download_loc,shell=True)
     print("done thread"+ str(page_num))
 
+#thread pool enviornment that starts the batches 
 def thread_start(page_cnt):
 
     if not os.path.exists(DEFAULT_LOCATION):
         os.makedirs(DEFAULT_LOCATION)
 
-    print(str(page_cnt))
+    print("starting thread pool. queue length:"+str(page_cnt))
     with concurrent.futures.ThreadPoolExecutor(max_workers=THREAD_COUNT) as threadPool:
         for page in range(page_cnt):
             threadPool.submit(threading_process,page)
