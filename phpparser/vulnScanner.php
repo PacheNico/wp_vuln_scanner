565c053099ffcdcf93f0f27fe1eef6719b63f1e0
Kyle Martin <ksmartin@bu.edu>
2020-04-27 13:30:19 -0400

    added concatenation detection to vuln scanner

--- phpparser/vulnScanner.php
+++ phpparser/vulnScanner.php
@@ -1,4 +1,5 @@
 <?php
+// Import libraries
 require 'vendor/autoload.php';
 use PhpParser\Error;
 use PhpParser\NodeDumper;
@@ -9,90 +10,129 @@ use PhpParser\NodeTraverser;
 use PhpParser\NodeVisitorAbstract;
 use PhpParser\NodeFinder;
 
-// test code ###########################################################
-$code = <<<'CODE'
-<?php
-if (PHP_SAPI === 'cli') {
-    parse_str(implode('&', array_slice($argv, 1)), $_GET);
-}
+// Scanner class 
+class sqlVulnScan extends NodeVisitorAbstract {
 
-$file_db = new PDO('sqlite:../database/database.sqlite');
-
-if (NULL == $_GET['id']) $_GET['id'] = 1;
-
-$sql = 'SELECT * FROM employees WHERE employeeId = ' . $_GET['id'];
-
-foreach ($file_db->query($sql) as $row) {
-    $employee = $row['LastName'] . " - " . $row['Email'] . "\n";
-
-    echo $employee;
-}
-?>
-CODE;
-// #####################################################################
-
-// extending NodeVisitorAbstract to store the parent class
-class ParentConnector extends NodeVisitorAbstract {
     private $stack;
-    public function beforeTraverse(array $nodes) {
-        $this->stack = [];
-    }
-    public function enterNode(Node $node) {
-        if (!empty($this->stack)) {
-            echo "test";
-            $node->setAttribute('parent', $this->stack[count($this->stack)-1]);
+    public $sqlVar;
+    private $varCheck;
+
+    // Sets class to either check the SQL statement or find the SQL variable
+    function __construct($a1 = "") {
+        $this->sqlVar = $a1;
+        if (strlen($a1) > 0) {
+            $this->varCheck = 1;
+        } else {
+            $this->varCheck = 0;
         }
-        $this->stack[] = $node;
     }
-    public function leaveNode(Node $node) {
-        array_pop($this->stack);
-    }
-}
 
-// obtain AST
-$parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
-try {
-    $ast = $parser->parse($code);
-} catch (Error $error) {
-    echo "Parse error: {$error->getMessage()}\n";
-    return;
-}
-
-// begin traverser
-$traverser = new NodeTraverser();
-$traverser->addVisitor(new class extends NodeVisitorAbstract {
-
-    private $stack;
+    // Sets the stack to an empty array before traversal
     public function beforeTraverse(array $nodes) {
         $this->stack = [];
     }
 
+    // Runs checks for SQL variables or SQL statements
     public function enterNode(Node $node) {
-        // $dumper = new NodeDumper;
-        // echo $dumper->dump($node) . "\n\n\n";
 
+        // Stores parent node
         if (!empty($this->stack)) {
             $node->setAttribute('parent', $this->stack[count($this->stack)-1]);
         }
         $this->stack[] = $node;
 
-        if ($node instanceof Node\Identifier && $node->name == "query") {
-            $queryName = $node->name;
-            $queryArgs = $node->getAttribute('parent')->args;
-            $varName = array_values($queryArgs)[0]->value->name;
-            echo $varName . "\n";
+        // SQL statement check
+        if ($this->varCheck == 1) {
+            if ($node instanceof Node\Expr\Assign){
+                if ($node->var instanceof Node\Expr\Variable){
+                    if ($this->sqlVar == $node->var->name) {
+                        $parent = $node->getAttribute('parent');
+                        if ($parent->expr->expr instanceof Node\Expr\BinaryOp\Concat) {
+                            echo "VULNERABILITY DETECTED: SQL query formed by concatenation" . "\n";
+                        }
+                    }
+                }
+            }
+        // SQL variable location
+        } else {
+            if ($node instanceof Node\Identifier && $node->name == "query") {
+                $queryName = $node->name;
+                $queryArgs = $node->getAttribute('parent')->args;
+                $varName = array_values($queryArgs)[0]->value->name;
+                $this->sqlVar = $varName;
+            }
         }
     }
 
+    // Pops the node
     public function leaveNode(Node $node) {
         array_pop($this->stack);
     }
 
-});
+}
 
-// traverse AST
-$dumper = new NodeDumper;
-$ast = $traverser->traverse($ast);
-// echo $dumper->dump($ast) . "\n";
+// Gets the source code of all PHP files in a directory
+function getDirContents($dir, &$results = array()) {
+    $files = scandir($dir);
+
+    foreach ($files as $key => $value) {
+        $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
+        $characterCount = strlen ( $path );
+        if (!is_dir($path)) {
+            if(substr($path, $characterCount-3, $characterCount) == "php"){
+                $results[] = $path;
+            }
+        } else if ($value != "." && $value != "..") {
+            getDirContents($path, $results);
+            if(substr($path, $characterCount-3, $characterCount) == "php"){
+                $results[] = $path;
+            }
+        }
+    }
+
+    return $results;
+}
+
+// Runs vulnerability scanner
+function parser($directory){
+    $all_php_paths = getDirContents($directory);
+
+    foreach ($all_php_paths as $key => $value) {
+        // extending NodeVisitorAbstract to store the parent class
+        echo "\n" . "Filename: " . $value . "\n";
+        $links_contents = file_get_contents($value);
+        $code = $links_contents;
+
+        // obtain AST
+        $parser = (new ParserFactory)->create(ParserFactory::PREFER_PHP7);
+        try {
+            $ast = $parser->parse($code);
+        } catch (Error $error) {
+            echo "Parse error: {$error->getMessage()}\n";
+            return;
+        }
+
+        $traverser1 = new NodeTraverser();
+        $traverser2 = new NodeTraverser();
+
+        // traverse AST to find sqlVars
+        $vuln1 = new SQLVulnScan();
+        $traverser1->addVisitor ($vuln1);
+        $ast1 = $traverser1->traverse($ast);
+        $sqlVarArray = $vuln1->sqlVar;
+
+        //traverse AST to find sql statements
+        $vuln2 = new SQLVulnScan($sqlVarArray);
+        $traverser2->addVisitor ($vuln2);
+        $ast2 = $traverser2->traverse($ast);
+    }
+
+}
+
+
+parser($argv[1]);
+
+
+
 
 
