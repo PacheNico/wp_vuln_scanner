b51adb65451ef564e7de9c9baefc9466b90caaad
Sharith Godamanna <sharithg@bu.edu>
2020-04-28 23:10:47 -0400

    modularized parser and scanner

---
+++ phpparser/SQLParser.php
@@ -0,0 +1,86 @@
+<?php
+// Import libraries
+require 'vendor/autoload.php';
+use PhpParser\Error;
+use PhpParser\NodeDumper;
+use PhpParser\ParserFactory;
+use PhpParser\Node;
+use PhpParser\Node\Stmt\Function_;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitorAbstract;
+use PhpParser\NodeFinder;
+
+// Scanner class 
+class sqlVulnScan extends NodeVisitorAbstract {
+
+    private $stack;
+    public $sqlVars;
+    private $varCheck;
+    public $sqlStatements;
+    public $isVuln = false;
+    public $lines;
+
+    // Sets class to either check the SQL statement or find the SQL variable
+    function __construct($a1=[]) {
+        $this->sqlVars = $a1;
+        $this->sqlStatements = [];
+        if (count($a1) == 0) {
+            $this->varCheck = 0;
+        } else {
+            $this->varCheck = 1;
+        }
+    }
+
+    // Sets the stack to an empty array before traversal
+    public function beforeTraverse(array $nodes) {
+        $this->stack = [];
+    }
+
+    // Runs checks for SQL variables or SQL statements
+    public function enterNode(Node $node) {
+
+        // Stores parent node
+        if (!empty($this->stack)) {
+            $node->setAttribute('parent', $this->stack[count($this->stack)-1]);
+        }
+        $this->stack[] = $node;
+
+        // SQL statement check
+        if ($this->varCheck == 1) {
+            if ($node instanceof Node\Expr\Assign){
+                if ($node->var instanceof Node\Expr\Variable){
+                    for ($i = 0; $i < sizeof($this->sqlVars); $i++) {
+                        if ($this->sqlVars[$i] == $node->var->name) {
+                            $parent = $node->getAttribute('parent');
+                            if ($parent->expr->expr instanceof Node\Expr\BinaryOp\Concat) {
+                                $this->isVuln = true;
+                                $this->line = $node->getLine();
+                                array_push($this->sqlStatements, $parent->expr->expr->left->value);
+                            }
+                        }
+                    }
+                }
+            }
+        // SQL variable location
+        } else{
+            if ($node instanceof Node\Identifier && $node->name == "query") {
+                $queryName = $node->name;
+                $queryArgs = $node->getAttribute('parent')->args; 
+                $dumper = new NodeDumper;      
+                echo $dumper->dump($queryArgs) . "\n";      
+                $varName = array_values($queryArgs)[0]->value->name;
+   
+                $this->sqlVar = $varName;
+                array_push($this->sqlVars, $varName);
+            }
+        }
+    }
+
+    // Pops the node
+    public function leaveNode(Node $node) {
+        array_pop($this->stack);
+    }
+
+}
+
+?>
--- phpparser/vulnScanner.php
+++ phpparser/vulnScanner.php
@@ -1,85 +1,9 @@
 <?php
-// Import libraries
-require 'vendor/autoload.php';
-use PhpParser\Error;
-use PhpParser\NodeDumper;
+
 use PhpParser\ParserFactory;
-use PhpParser\Node;
-use PhpParser\Node\Stmt\Function_;
 use PhpParser\NodeTraverser;
-use PhpParser\NodeVisitorAbstract;
-use PhpParser\NodeFinder;
+require_once('SQLParser.php');
 
-// Scanner class 
-class sqlVulnScan extends NodeVisitorAbstract {
-
-    private $stack;
-    public $sqlVars;
-    private $varCheck;
-    public $sqlStatements;
-    public $isVuln = false;
-
-    // Sets class to either check the SQL statement or find the SQL variable
-    function __construct($a1=[]) {
-        $this->sqlVars = $a1;
-        $this->sqlStatements = [];
-        if (count($a1) == 0) {
-            $this->varCheck = 0;
-        } else {
-            $this->varCheck = 1;
-        }
-    }
-
-    // Sets the stack to an empty array before traversal
-    public function beforeTraverse(array $nodes) {
-        $this->stack = [];
-    }
-
-    // Runs checks for SQL variables or SQL statements
-    public function enterNode(Node $node) {
-
-        // Stores parent node
-        if (!empty($this->stack)) {
-            $node->setAttribute('parent', $this->stack[count($this->stack)-1]);
-        }
-        $this->stack[] = $node;
-
-        // SQL statement check
-        if ($this->varCheck == 1) {
-            if ($node instanceof Node\Expr\Assign){
-                if ($node->var instanceof Node\Expr\Variable){
-                    for ($i = 0; $i < sizeof($this->sqlVars); $i++) {
-                        if ($this->sqlVars[$i] == $node->var->name) {
-                            $parent = $node->getAttribute('parent');
-                            if ($parent->expr->expr instanceof Node\Expr\BinaryOp\Concat) {
-                                $this->isVuln = true;
-                                array_push($this->sqlStatements, $parent->expr->expr->left->value);
-                            }
-                        }
-                    }
-                }
-            }
-        // SQL variable location
-        } else{
-            if ($node instanceof Node\Identifier && $node->name == "query") {
-                $queryName = $node->name;
-                $queryArgs = $node->getAttribute('parent')->args; 
-                $dumper = new NodeDumper;      
-                echo $dumper->dump($queryArgs) . "\n";      
-                $varName = array_values($queryArgs)[0]->value->name;
-   
-                $this->sqlVar = $varName;
-                array_push($this->sqlVars, $varName);
-            }
-        }
-    }
-
-    // Pops the node
-    public function leaveNode(Node $node) {
-        array_pop($this->stack);
-    }
-
-}
 
 // Gets the source code of all PHP files in a directory
 function getDirContents($dir, &$results = array()) {
@@ -103,22 +27,11 @@ function getDirContents($dir, &$results = array()) {
     return $results;
 }
 
-function getLineWithString($fileName, $str) {
-    $lines = file($fileName);
-    $idx = 1;
-    foreach ($lines as $lineNumber => $line) {
-        if (strpos($line, $str) !== false) {
-            return $idx;
-        }
-        $idx++;
-    }
-    return -1;
-}
-
 // Runs vulnerability scanner
 function parser($directory){
     $all_php_paths = getDirContents($directory);
 
+    // iterating through all php paths
     foreach ($all_php_paths as $key => $value) {
         // extending NodeVisitorAbstract to store the parent class
         echo "\n" . "Scanning file " . str_replace(getcwd().'/', "", $value) . "\n";
@@ -155,7 +68,7 @@ function parser($directory){
 
         if($isVuln){
             echo "\tWARNING, Concatenating SQL statement detected, Possible SQL Injection\n";
-            $line = getLineWithString($value, $statement);
+            $line = $as2->line;
             echo "\tFound in line ".$line." of ".$value;
 
             for ($i = 0; $i < sizeof($statements); $i++) {
