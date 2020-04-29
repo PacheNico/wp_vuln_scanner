a13c00d2b59a3bed1000010c4ea33b5c6d9642fb
NicoPache <mnpache@gmail.com>
2020-04-29 00:53:15 -0400

    merge

--- phpparser/Parser.php
+++ phpparser/Parser.php
@@ -134,13 +134,9 @@ class sqlVulnScan extends NodeVisitorAbstract {
                 return $return_node;    // return_node is node with subnode that matches a sink
             }
         }
-<<<<<<< HEAD
-        if (!empty($return_node)){return $return_node; }// NULL
-=======
         if (!empty($return_node)){
             return $return_node;    // return_node is node with subnode that matches a sink
         } // NULL
->>>>>>> 24657d8038e1f99ddd2c82712f82497cf416d815
 
     }
 
